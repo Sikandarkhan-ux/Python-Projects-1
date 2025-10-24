df43f8f66d6aea7694372bbfd92e53bc525caf41
Sikandarkhan-ux <sikandrkhn82@gmail.com>
2025-10-24 18:49:00 +0500

    Add files via upload

---
+++ Python addition calc.py
@@ -0,0 +1,4 @@
+number_1 = (input("Enter the first number: "))
+number_2 = (input("Enter the second number: "))
+result =  float(number_1)+ float(number_2)
+print(result)
---
+++ SImple Madlib.py
@@ -0,0 +1,8 @@
+name = input("Enter the name: ")
+gender = input("Enter the gender(girl or boy): ")
+verb = input("Enter the gerund: ")
+adjective = input("Enter the adjective: ")
+
+print(name + " is a bad " + gender )
+print(name + " is " + verb)
+print(name + " is very " + adjective)
